3ebf3b4aa6a8e9319385d67008e24e2deb9554e6
wll <836601745@qq.com>
2025-05-21 15:32:55 +0800

    第一次修改

--- src/server/services/BornUnit.ts
+++ src/server/services/BornUnit.ts
@@ -4,7 +4,5 @@ import { HunterUnit } from "shared/UnitTypes";
 @Service({})
 export class BornUnit implements OnStart {
 	private spawnLocation = new Vector3(0, 5, 0); // 生成位置，可以根据需要修改
-	onStart() {
-		task.spawn(() => {});
-	}
+	onStart() {}
 }
